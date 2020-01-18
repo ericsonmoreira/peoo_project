0a3688411bec712eb89de3fe6c57791eef7a720d
Ericson Moreira <moreira.ericson@gmail.com>
2020-01-18 18:45:45 -0200

    Resolvendo problemas com conversão.

--- PEOOProject/src/lesson_02/questions/Question_01.java
+++ PEOOProject/src/lesson_02/questions/Question_01.java
@@ -6,11 +6,11 @@ import util.Question;
  * 
  * @author Ericson Rogerio Moreira
  * 
- * ExercÃ­cio 1
+ * Exercício 1
  * 
- * 1) FaÃ§a um programa que receba dois nÃºmeros reais (double), calcule e mostre a mÃ©dia entre eles.
+ * 1) Faça um programa que receba dois números reais (double), calcule e mostre a mÃ©dia entre eles.
  * 
- * 2) Caso a mÃ©dia seja maior que 7, mostrar a mensagem â€œAprovadoâ€�, senÃ£o mostrar â€œReprovadoâ€�.
+ * 2) Caso a média seja maior que 7, mostrar a mensagem Aprovado, se não mostrar Reprovado.
  *
  */
 
@@ -18,18 +18,18 @@ public class Question_01 extends Question {
 
 	public static void main(String[] args) {
 		
-		// Pegando o primeiro nÃºmero
-		System.out.println("Digite o primeiro nÃºmero: ");
+		// Pegando o primeiro número
+		System.out.println("Digite o primeiro númmero: ");
 		double a = scanner.nextDouble();
 		
-		// Pegando o segundo nÃºmero.
-		System.out.println("Digite o primeiro nÃºmero: ");
+		// Pegando o segundo número.
+		System.out.println("Digite o primeiro númmero: ");
 		double b = scanner.nextDouble();
 		
-		// MÃ©dia
+		// Média
 		double mean = (a + b)/2;
 		
-		System.out.println("MÃ©dia: " + mean + ".");
+		System.out.println("Média: " + mean + ".");
 		
 		if (mean > 7) {
 			System.out.println("Aprovado.");
