cc4fc8ae9ef1a8731f2b821dc001fc36b0e20534
Ericson Moreira <moreira.ericson@gmail.com>
2020-01-18 18:41:18 -0200

    ALTERANDO DO ECLIPSE.

--- PEOOProject/src/lesson_02/questions/Question_01.java
+++ PEOOProject/src/lesson_02/questions/Question_01.java
@@ -6,11 +6,11 @@ import util.Question;
  * 
  * @author Ericson Rogerio Moreira
  * 
- * Exercício 1
+ * ExercÃ­cio 1
  * 
- * 1) Faça um programa que receba dois números reais (double), calcule e mostre a média entre eles.
+ * 1) FaÃ§a um programa que receba dois nÃºmeros reais (double), calcule e mostre a mÃ©dia entre eles.
  * 
- * 2) Caso a média seja maior que 7, mostrar a mensagem “Aprovado”, senão mostrar “Reprovado”.
+ * 2) Caso a mÃ©dia seja maior que 7, mostrar a mensagem â€œAprovadoâ€�, senÃ£o mostrar â€œReprovadoâ€�.
  *
  */
 
@@ -18,18 +18,18 @@ public class Question_01 extends Question {
 
 	public static void main(String[] args) {
 		
-		// Pegando o primeiro número
-		System.out.println("Digite o primeiro número: ");
+		// Pegando o primeiro nÃºmero
+		System.out.println("Digite o primeiro nÃºmero: ");
 		double a = scanner.nextDouble();
 		
-		// Pegando o segundo número.
-		System.out.println("Digite o primeiro número: ");
+		// Pegando o segundo nÃºmero.
+		System.out.println("Digite o primeiro nÃºmero: ");
 		double b = scanner.nextDouble();
 		
-		// Média
+		// MÃ©dia
 		double mean = (a + b)/2;
 		
-		System.out.println("Média: " + mean + ".");
+		System.out.println("MÃ©dia: " + mean + ".");
 		
 		if (mean > 7) {
 			System.out.println("Aprovado.");
@@ -37,6 +37,7 @@ public class Question_01 extends Question {
 			System.out.println("Reprovado.");
 		}
 		// Testando o Git.
+		// Alterado do Eclipse.
 
 	}
 
