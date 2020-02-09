98cfa71575fb95779ee5e2acb0e6bb85ae26e07f
Ericson Moreira <moreira.ericson@gmail.com>
2020-02-09 10:30:32 -0300

    Testando as funções lambda do java.

--- PEOOProject/src/util/Testando.java
+++ PEOOProject/src/util/Testando.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * Classe so para testes rápidos.
- * <p>
+ *
  * Como usar "method reference" e forEach.
  *
- * @author Ericson R. Moreira {@link ericson.moreira@aluno.uece.br }
  */
 public class Testando {
 
@@ -23,15 +23,10 @@ public class Testando {
         scanner = new Scanner(System.in);
         String string = scanner.nextLine();
         System.out.println(string);
-
-
     }
 
     public static void main(String[] args) {
-
 //		testeScanner();
-
-
         List<Integer> vetor = new ArrayList<Integer>();
 
         for (int i = 0; i < 100; i++) {
@@ -39,6 +34,7 @@ public class Testando {
         }
 
         // Imprime todos os elementos da Lista.
+        // vetor.forEach(System.out::println); // Também pode fazer assim.
         vetor.forEach(v -> System.out.println(v));
 
         // Imprime todos os elementos da Lista.
@@ -48,7 +44,9 @@ public class Testando {
         // filtra os elementos do vetor que são maiores ou iguais a 50 e imprime.
         // o metoso stream() retorna um stream dos elementos da lista
         vetor.stream().filter(v -> v >= 50).forEach(v -> System.out.println("[" + v + "]"));
+        vetor.stream().sorted().forEach(integer -> System.out.println(integer));
 
+        vetor.stream().map(integer -> integer * 2).filter(integer -> integer > 100).forEach(integer -> System.out.println(integer));
 
     }
 
